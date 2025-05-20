684a322024a2357b018f9610bf1585941eb6bf7b
alexandrosadam <alexadam340@yahoo.com>
2025-05-20 20:59:46 +0300

    Rename file paths in main, fix ontology schema, add manufacturedBy relation and include it in mappings file and remove unnecessary files from repo

--- src/main/java/org/example/RMLPipeline.java
+++ src/main/java/org/example/RMLPipeline.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 
 public final class RMLPipeline {
     static String rootFolder = "./src/main/resources/";
-    static String mappingPathFile = rootFolder + "car-sales-mappings.ttl";
-    static String convertedCarOntology = rootFolder + "car_sales_ontology_turtle_format_converted.ttl";
-    static String rmlRulesMappingsOutput = rootFolder + "rml_rules_mappings_output.ttl";
+    static String mappingRMLRulesFile = rootFolder + "car-sales-mappings.ttl";
+    static String carSalesOntologyFile = rootFolder + "car_sales_ontology.owl";
+    static String outputMappingRMLRulesFile = rootFolder + "rml_rules_mappings_output.ttl";
 
     public static void main(String[] args) throws IOException {
         GraphDBConnection.initializeRepository(); // Initializes the connection to the GraphDB repository
 
-        GraphDBConnection.addRDFFileToGraphDB(convertedCarOntology); // Insert the owl ontology (converted to Turtle format) file into the repository
+        GraphDBConnection.addRDFFileToGraphDB(carSalesOntologyFile); // Insert the owl ontology (converted to Turtle format) file into the repository
 
-        Utils.executeRMLMapper(mappingPathFile, rmlRulesMappingsOutput); // Execute RML pipeline to apply RML rules on car sales dataset
+        Utils.executeRMLMapper(mappingRMLRulesFile, outputMappingRMLRulesFile); // Execute RML pipeline to apply RML rules on car sales dataset
 
-        GraphDBConnection.addRDFFileToGraphDB(rmlRulesMappingsOutput); // Insert output file after mapping RLM rules on car sales dataset into the repository
+        GraphDBConnection.addRDFFileToGraphDB(outputMappingRMLRulesFile); // Insert output file after mapping RLM rules on car sales dataset into the repository
 
         Utils.executePipelineOfSPARQLQueries(); // Execute pipeline of SPARQL queries
 
